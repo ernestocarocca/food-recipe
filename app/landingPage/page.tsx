93a961ba3525773f7544f998a9af1dfa09da141c
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2024-09-02 23:04:12 +0200

    dd

--- app/landingPage/page.tsx
+++ app/landingPage/page.tsx
@@ -1,10 +1,10 @@
-import CanvasRevealEffectDemo from '@/components/example/canvas-reveal-effect-demo'
+
 import React from 'react'
 
 const LandingPage = () => {
     return (
         <div>
-            <CanvasRevealEffectDemo />
+    
         </div>
     )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "alexander-redl-d3bYmnZ0ank-unsplash.jpg"
+    ]
+  }
+};
+
 
 export default nextConfig;
+// next.config.js
